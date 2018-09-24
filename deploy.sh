a3e73aea8ad14f1488be84504852100ff05d6e47
yunzhuqing <june91914@gmail.com>
2018-09-24 10:11:03 +0800

    修复部署脚本

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ TOMCAT_PATH=/usr/local/share/apache-tomcat-8.0.53/wwebapps
 APP_NAME=platform-framework
 rm -rf ${APP_PATH}/${APP_NAME}.war
 rm -rf ${APP_PATH}/${APP_NAME}
-cp ./platform-shop/platform-shop-1.0.0.war ${TOMCAT_PATH}/${APP_NAME}.war
+cp ./platform-shop/target/platform-shop-1.0.0.war ${TOMCAT_PATH}/${APP_NAME}.war
