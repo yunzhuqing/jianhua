d1df6db1bdd9ae42b8472f33cf4a371518168237
yunzhuqing <june91914@gmail.com>
2018-09-24 10:07:47 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-TOMCAT_PATH=/usr/local/share/apache-tomcat-8.0.53
+TOMCAT_PATH=/usr/local/share/apache-tomcat-8.0.53/wwebapps
 APP_NAME=platform-framework
 rm -rf ${APP_PATH}/${APP_NAME}.war
 rm -rf ${APP_PATH}/${APP_NAME}
+cp ./platform-shop/platform-shop-1.0.0.war ${TOMCAT_PATH}/${APP_NAME}.war
