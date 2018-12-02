33b8c947240912414a181ec4c321aa55d19671a4
yunzhuqing <june91914@gmail.com>
2018-12-02 23:02:12 +0800

    引入springboot

--- wx-mall/config/api.js
+++ wx-mall/config/api.js
@@ -1,5 +1,5 @@
-// var NewApiRootUrl = 'http://47.75.86.219:8080/platform-framework/api/';
-var NewApiRootUrl = 'http://localhost:8080/api/';
+var NewApiRootUrl = 'http://www.huashili.io:8080/api/';
+// var NewApiRootUrl = 'http://localhost:8080/api/';
 module.exports = {
   IndexUrlNewGoods: NewApiRootUrl + 'index/newGoods', //
   IndexUrlHotGoods: NewApiRootUrl + 'index/hotGoods', //首页数据接口
