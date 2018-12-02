095002dfe2e69d997e78d4c3c3683d82a38aced1
yunzhuqing <june91914@gmail.com>
2018-12-02 23:01:53 +0800

    引入spring boot

---
+++ platform-shop/src/main/java/com/platform/PlatformShopApplication.java
@@ -0,0 +1,12 @@
+package com.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformShopApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(PlatformShopApplication.class);
+    }
+}
--- platform-shop/src/main/java/com/platform/PropertiesReload.java
+++ platform-shop/src/main/java/com/platform/PropertiesReload.java
@@ -1,7 +1,5 @@
 package com.platform;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
