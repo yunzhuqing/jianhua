2d000956a09759ab15268b99e6febe254746eb74
yunzhuqing <june91914@gmail.com>
2018-12-26 22:16:20 +0800

    添加logback日志

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ rm -rf ${APP_PATH}/${APP_NAME}.jar
 echo "rm ${TOMCAT_PATH}/${APP_NAME}.jar"
 rm -rf ${APP_PATH}/${APP_NAME}
 echo "rm ${APP_PATH}/${APP_NAME}"
-cp ./platform-shop/target/platform-shop-1.0.0.jar ${APP_PATH}/${APP_NAME}.war
+cp ./platform-shop/target/platform-shop-1.0.0.jar ${APP_PATH}/${APP_NAME}.jar
