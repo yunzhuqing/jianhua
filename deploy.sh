5db52b0f86078e4d2495745338e7ea92fee6377a
yunzhuqing <june91914@gmail.com>
2018-12-26 22:12:48 +0800

    添加logback日志

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 git pull
 mvn clean package -Dmaven.test.skip=true
-TOMCAT_PATH=/usr/local/share/apache-tomcat-8.0.53/webapps
+APP_PATH=/root/data
 APP_NAME=platform-framework
-rm -rf ${APP_PATH}/${APP_NAME}.war
-echo "rm ${TOMCAT_PATH}/${APP_NAME}.war"
+rm -rf ${APP_PATH}/${APP_NAME}.jar
+echo "rm ${TOMCAT_PATH}/${APP_NAME}.jar"
 rm -rf ${APP_PATH}/${APP_NAME}
-echo "rm ${TOMCAT_PATH}/${APP_NAME}"
-cp ./platform-shop/target/platform-shop-1.0.0.war ${TOMCAT_PATH}/${APP_NAME}.war
+echo "rm ${APP_PATH}/${APP_NAME}"
+cp ./platform-shop/target/platform-shop-1.0.0.jar ${APP_PATH}/${APP_NAME}.war
